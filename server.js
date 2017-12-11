de21495e0fa3546b2b8e66fd8fd9efec4c9065c0
samuelssnider <sam_redins@yahoo.com>
2017-12-11 09:42:50 -0700

    initial commit, server setup and first test passing :)

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+
+app.set('port', process.env.PORT || 3000)
+app.locals.title = "Secret Box"
+
+app.get('/', function(request, response) {
+  response.send('Spooky')
+})
+
+if(!module.parent){
+  app.listen('port', function() {
+    console.log(`${app.locals.title} is running on port ${app.get('port')}`)
+  })
+}
+
+module.exports = app
