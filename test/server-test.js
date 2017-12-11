1544ce06fc798bccd847ae9a97a4a5cc35b00fc7
samuelssnider <sam_redins@yahoo.com>
2017-12-11 10:04:20 -0700

    second test passing

--- test/server-test.js
+++ test/server-test.js
@@ -1,9 +1,33 @@
-var assert = require('chai').assert;
-var app    = require('../server');
+var assert  = require('chai').assert;
+var app     = require('../server');
+var request = require('request')
 
 describe('Server', function() {
+  before(function(done) {
+    this.port = 9876;
+    this.server = app.listen(this.port, function(err, result) {
+      if(err) {return done(err); }
+      done();
+    });
+    this.request = request.defaults({baseUrl: 'http://localhost:9876'});
+  });
+  
+  after(function() {
+    this.server.close();
+  });
   
   it('should exist', function() {
     assert(app);
   });
+  
+  describe('GET /', function() {
+    it('should return a 200', function(done) {
+      this.request.get('/', function(error, response) {
+        if(error) {done(error)}
+        assert.equal(response.statusCode, 200);
+        done()
+      });
+    })
+  });
+  
 });
